4ee75867256f4fc5a57dda4134d7988518c40dbd
devJeremie <chabanaisjeremie@gmail.com>
2024-10-09 12:25:48 +0200

    HomePage

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
+  <header>
+    <nav>
+      <RouterLink to="/">
+        <img src="/youtube.png" alt="logo you tube">
+      </RouterLink>
+      <form >
+        <input type="text" placeholder="Recherche...">
+      </form>
+      <font-awesome-icon icon="magnifying-glass" />
+    </nav>
+  </header>
   <RouterView />
 </template>
 
