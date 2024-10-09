664c359d424e4cf93b2076a0bd95e04341374fcf
devJeremie <chabanaisjeremie@gmail.com>
2024-10-09 10:25:53 +0200

    TailwindInstall

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <template>
-  <h1>Bienvenue sur le projet Vue.js</h1>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RouterView />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.vue'],  //n'importe quel fichier qui est dans src et qui finit par .vue
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
